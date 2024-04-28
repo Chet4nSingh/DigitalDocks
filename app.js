705edb4bbc13bef744158bec4b6b39df0ffd8b57
Chetan Singh <chet4nsingh@gmail.com>
2024-04-28 15:23:01 +0530

    Session & Flash

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./helpers/ExpressError');
 const catchAsync = require('./helpers/catchAsync');
 const Product = require('./models/product');
 const Cart = require('./models/cart');
 const passport = require('passport');
 const localStrategy = require('passport-local');
+const store = require('./routes/store');
 
 mongoose
   .connect("mongodb://localhost/ecommerce-project")
@@ -25,64 +28,35 @@ app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 app.use(methodOverride('_method'));
+
+const sessionConfig = {
+  secret: 'thisisasecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
+app.use('/store', store);
 
 app.get('/home', async (req, res) => {
     const products = await Product.find({ rating: { $gte: 4 }}, null, { limit: 8 });
     res.render('shop/home', { products });
 })
-
-app.get('/store', catchAsync(async (req, res) => {
-    const products = await Product.find();    
-    res.render('shop/store', { products, categoryId: 'all' });
-}));
-
-app.get('/store/cart',  catchAsync(async (req, res) => {
-  const products = await Cart.find().populate('product');
-  res.render('shop/cart', { products });
-}));
-
-app.get('/store/:categoryId', catchAsync(async (req, res) => {
-    const { categoryId } = req.params;
-    const products = await Product.find({title: `${categoryId.substring(0, categoryId.length - 1)}`});
-    if (products.length === 0) throw new ExpressError('No products found', 400);
-    res.render('shop/store', { products, categoryId });
-}));
-
-app.get('/store/:categoryId/:productId', catchAsync(async (req, res) => {
-    const { productId } = req.params;
-    const product = await Product.findById(productId);
-    res.render('shop/details', { product });
-}));
-
-app.post('/store', catchAsync(async (req, res) => {
-    const { productId } = req.body;        
-    if (await Cart.exists({ product: productId })) {
-      const inCart = await Cart.findOne({ product: productId });
-      inCart.quantity += 1;
-      await inCart.save();
-    } else {   
-      const product = await Product.findById(productId);
-      const addedProduct = await new Cart({ product, quantity: 1 });
-      await addedProduct.save();
-    }
-    res.redirect(`/store/cart`);
-}));
-
-app.delete('/store/cart', async(req, res) => {
-  const { cartId } = req.body;
-  const product = await Cart.findById(cartId);
-  if (product.quantity > 1) {
-    product.quantity -= 1;
-    await product.save();
-  } else {
-    await Cart.findByIdAndDelete(cartId);
-  }
-  res.redirect('/store/cart');
-});
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404));
@@ -98,4 +72,3 @@ app.listen(3000, () => {
     console.log('listening...');
 });
 
-// Note: Add to cart feature!!
---
+++ routes/store.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const ExpressError = require('../helpers/ExpressError');
+const catchAsync = require('../helpers/catchAsync');
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+router.get('/', catchAsync(async (req, res) => {
+    const products = await Product.find();    
+    res.render('shop/store', { products, categoryId: 'all' });
+}));
+
+router.get('/cart',  catchAsync(async (req, res) => {
+  const products = await Cart.find().populate('product');
+  res.render('shop/cart', { products });
+}));
+
+router.get('/:categoryId', catchAsync(async (req, res) => {
+    const { categoryId } = req.params;
+    const products = await Product.find({title: `${categoryId.substring(0, categoryId.length - 1)}`});
+    if (products.length === 0) throw new ExpressError('No products found', 400);
+    res.render('shop/store', { products, categoryId });
+}));
+
+router.get('/:categoryId/:productId', catchAsync(async (req, res) => {
+    const { categoryId, productId } = req.params;
+    const product = await Product.findById(productId);
+    if (!product) {
+        req.flash('error', 'Product does not exist');
+        return res.redirect(`/store/${ categoryId }`);
+    }
+    res.render('shop/details', { product });
+}));
+
+router.post('', catchAsync(async (req, res) => {
+    const { productId } = req.body;        
+    if (await Cart.exists({ product: productId })) {
+      const inCart = await Cart.findOne({ product: productId });
+      inCart.quantity += 1;
+      await inCart.save();
+    } else {   
+      const product = await Product.findById(productId);
+      const addedProduct = await new Cart({ product, quantity: 1 });
+      await addedProduct.save();
+    }
+    req.flash('success', 'Product added to cart');
+    res.redirect(`/store/cart`);
+}));
+
+router.delete('/cart', async(req, res) => {
+  const { cartId } = req.body;
+  const product = await Cart.findById(cartId);
+  if (product.quantity > 1) {
+    product.quantity -= 1;
+    await product.save();
+  } else {
+    await Cart.findByIdAndDelete(cartId);
+  }
+  req.flash('success', 'Product removed from cart');
+  res.redirect('/store/cart');
+});
+
+module.exports = router;
